494cde9cd69dd47088003268a1eabf3fcd57357c
ArtemErmulin <artem.ermulin@ya.ru>
2020-11-29 09:28:21 -0800

    Add installing sublime text to linux_configure.sh

--- linux/linux_configure.sh
+++ linux/linux_configure.sh
@@ -57,3 +57,9 @@ source ~/.bashrc
 pyenv install 3.7.7
 pyenv global 3.7.7
 pip install --upgrade pip
+
+echo '### install sublime text ###'
+wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add
+sudo apt-add-repository "deb https://download.sublimetext.com/ apt/stable/"
+sudo apt install sublime-text
+
